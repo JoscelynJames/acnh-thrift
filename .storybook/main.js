da06ebf57eb2f2a2e0addfba93ccbac955ba97eb
Joscelyn James <josce.james7@hotmail.com>
2020-05-12 08:40:46 -0600

    chore: updated storybook and added typescript

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,8 @@
 module.exports = {
   stories: ['../src/**/*.stories.js'],
   addons: [
-    '@storybook/preset-create-react-app',
+    '@storybook/preset-typescript',
     '@storybook/addon-actions',
     '@storybook/addon-links',
-  ],
+  ]
 };
---
+++ src/stories/2-Header.stories.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Header from '../components/Header.tsx';
+import '../../node_modules/@elastic/eui/dist/eui_theme_light.min.css';
+
+export default {
+  title: 'Header',
+  component: Header,
+};
+
+export const Default = () => <Header />;
+
+export const LoggedIn = () => <Header isLoggedIn />;
+
+const breadcrumbs = [
+  {
+    text: 'peanut',
+    href: '/peanut',
+  },
+  {
+    text: 'butter',
+    href: '/butter',
+  },
+  {
+    text: 'jelly',
+    href: '/jelly',
+  },
+  {
+    text: 'sandwiches',
+  },
+];
+
+export const Breadcrumbs = () => <Header isLoggedIn breadcrumbs={breadcrumbs} />;
