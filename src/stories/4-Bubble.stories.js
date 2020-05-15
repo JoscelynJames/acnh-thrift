270a7244f05929a1f669de40de38f979f093512c
Joscelyn James <josce.james7@hotmail.com>
2020-05-14 18:39:53 -0600

    feat: added bubble component

---
+++ src/stories/4-Bubble.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Bubble from '../components/Bubble.tsx';
+import '../../node_modules/@elastic/eui/dist/eui_theme_light.min.css';
+
+export default {
+  title: 'Bubble',
+  component: Bubble,
+};
+
+export const Default = () => <Bubble />;
